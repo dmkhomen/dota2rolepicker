52d9718f071868742924990bf2f81fc545b78f8e
dmkhomen <dmkhomen@gmail.com>
2018-06-08 11:50:58 +0300

    v0.3.4 refactored hero filtration, divided functions

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Dota 2 Hero Role Picker
+## Dota 2 Hero Role Picker
+v0.3.4
+Check it out at [dmkhomen.github.io/dota2rolepicker/!](https://dmkhomen.github.io/dota2rolepicker/) (hosted with Github Pages)
+Made with HTML, CSS and vanilla Javascript.
 
-[Check it out! (hosted with Github Pages)](https://dmkhomen.github.io/dota2rolepicker/)
-
-## Description
+### Description
 This is a tool to help pick hero during strategy time. Use: filter by role, check out what damage type is needed (seek to balance physical and magical damage in your team), try to get melee-range pairs as core-supports on laning phase, filter by your favorite heroes if you have any (you have to add them by clicking on portrait).
 
-Made with HTML, CSS and vanilla Javascript.
+
--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function hasClass(element, classSearched) {
     return (' ' + element.className + ' ').indexOf(' ' + classSearched + ' ') > -1;
 }
 
-//function to set cookie (used to save favorite heroes)
+//set cookie (used to save favorite heroes)
 function setCookie(name, value, expirationDays) {
     var date = new Date();
     date.setTime(date.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
@@ -29,7 +29,7 @@ function arrayContainsArray(superset, subset) {
     });
 }
 
-//function to get cookie (used to get favorite heroes from cookie)
+//get cookie (used to get favorite heroes from cookie)
 function getCookie(cookieName) {
     var name = cookieName + "=";
     var cookieArray = document.cookie.split(';');
@@ -69,10 +69,10 @@ function mycallback(data) {
     var txt_str = '';
     var txt_agi = '';
     var txt_int = '';
+
     for (i in heroes) {
         var item = '<div data-id="' + heroes[i].id +
-            '" data-toggle="tooltip" data-placement="bottom" title="' +
-            heroes[i].localized_name + '" class="hero"><img src="http://cdn.dota2.com/apps/dota2/images/heroes/' +
+            '" class="hero"><img src="http://cdn.dota2.com/apps/dota2/images/heroes/' +
             heroes[i].name.substr(14, heroes[i].name.length) + '_lg.png"><i class="favorite-star fas fa-star"></i><i class="favorite-star far fa-star"></i></div>';
         if (heroes[i].attribute == 'str') {
             txt_str += item;
@@ -82,12 +82,9 @@ function mycallback(data) {
             txt_int += item;
         }
     }
-
     document.getElementById("demo_str").innerHTML = txt_str;
     document.getElementById("demo_agi").innerHTML = txt_agi;
     document.getElementById("demo_int").innerHTML = txt_int;
-
-
 
     //favorite heroes processing, if cookie exists, get values from it and add css class to favorite heroes in list
     var favorites = [];
@@ -109,7 +106,6 @@ function mycallback(data) {
     }
 
     //update favorites and cookie on hero clicking
-    //todo visualize number of favorite heroes
     for (var i = 0; i < items.length; i++) {
         items[i].addEventListener('click', function () {
             var id = this.getAttribute("data-id");
@@ -124,220 +120,55 @@ function mycallback(data) {
             totalFavorites = favorites.length;
             totalFavoritesSpan.innerHTML = totalFavorites;
             var favoritesCookie = JSON.stringify(favorites);
-            setCookie("favs", favoritesCookie, 365);
+            setCookie("favs", favoritesCookie, 3650);
         }, false);
-        items[i].addEventListener('click', selectHeroes, false);
+        items[i].addEventListener('click', showHeroes, false);
     }
 
     var selectors = document.getElementsByClassName("selector");
+
+    var favoritesToggled = false;
+    var featuresSelected = [];
+
     for (var i = 0; i < selectors.length; i++) {
-        selectors[i].addEventListener('click', selectHeroes, false);
+        selectors[i].addEventListener('click', selectFilters, false);
     }
 
-    //default values
-    var roleSelected = 'all';
-    var damageSelected = 'all';
-    var rangeSelected = 'all';
-    // var aoesplashSelected = 'all';
-    // var roamerSelected = 'all';
-    // var initiatorSelected = 'all';
-    // var disablerSelected = 'all';
-    // var nukerSelected = 'all';
-    // var saverSelected = 'all';
-    // var pusherSelected = 'all';
-    // var durableSelected = 'all';
-    var favoritesSelected = false;
+    function selectFilters() {
 
-    var specialsSelected = [];
-
-    function selectHeroes() {
-
-        var attribute = this.getAttribute("data-select");
         var value = this.getAttribute("data-value");
-        var alreadySelected;
 
         if (hasClass(this, "selected")) {
-            console.log("already selected");
-            alreadySelected = true;
-        }
-        var clickedGroup = document.querySelectorAll('[data-select="' + attribute + '"]');
-        for (var i = 0; i < clickedGroup.length; i++) {
-            // console.log(clickedGroup[i].getAttribute("data-select"));
-            if (!(clickedGroup[i].getAttribute("data-select") == 'specials')) {
-                clickedGroup[i].classList.remove("selected");
+            var index = featuresSelected.indexOf(value);
+            if (this.getAttribute("data-value") == "favorites") {
+                favoritesToggled = false;
+            } else {
+                featuresSelected.splice(index, 1);
             }
-        }
-        switch (this.getAttribute("data-select")) {
-            case 'role':
-                if (alreadySelected) {
-                    roleSelected = 'all';
-                    this.classList.remove("selected");
-                } else {
-                    roleSelected = value;
-                    this.classList.add("selected");
-                }
-                break;
-            case 'damage':
-                if (alreadySelected) {
-                    damageSelected = 'all';
-                    this.classList.remove("selected");
-                } else {
-                    damageSelected = value;
-                    this.classList.add("selected");
-                }
-                break;
-            case 'range':
-                if (alreadySelected) {
-                    rangeSelected = 'all';
-                    this.classList.remove("selected");
-                } else {
-                    rangeSelected = value;
-                    this.classList.add("selected");
-                }
-                break;
-            case 'favorites':
-                if (alreadySelected) {
-                    favoritesSelected = false;
-                    this.classList.remove("selected");
-                } else {
-                    favoritesSelected = value;
-                    this.classList.add("selected");
-                }
-                break;
-                // case 'aoesplash':
-                //     if (alreadySelected) {
-                //         aoesplashSelected = 'all';
-                //         // this.classList.remove("selected");
-                //     } else {
-                //         aoesplashSelected = value;
-                //         this.classList.add("selected");
-                //     }
-                //     break;
-                // case 'roamer':
-                //     if (alreadySelected) {
-                //         roamerSelected = 'all';
-                //         // this.classList.remove("selected");
-                //     } else {
-                //         roamerSelected = value;
-                //         this.classList.add("selected");
-                //     }
-                //     break;
-                // case 'initiator':
-                //     if (alreadySelected) {
-                //         initiatorSelected = 'all';
-                //         // this.classList.remove("selected");
-                //     } else {
-                //         initiatorSelected = value;
-                //         this.classList.add("selected");
-                //     }
-                //     break;
-                // case 'disabler':
-                //     if (alreadySelected) {
-                //         disablerSelected = 'all';
-                //         // this.classList.remove("selected");
-                //     } else {
-                //         disablerSelected = value;
-                //         this.classList.add("selected");
-                //     }
-                //     break;
-                // case 'nuker':
-                //     if (alreadySelected) {
-                //         nukerSelected = 'all';
-                //         // this.classList.remove("selected");
-                //     } else {
-                //         nukerSelected = value;
-                //         this.classList.add("selected");
-                //     }
-                //     break;
-                // case 'pusher':
-                //     if (alreadySelected) {
-                //         pusherSelected = 'all';
-                //         // this.classList.remove("selected");
-                //     } else {
-                //         pusherSelected = value;
-                //         this.classList.add("selected");
-                //     }
-                //     break;
-                // case 'durable':
-                //     if (alreadySelected) {
-                //         durableSelected = 'all';
-                //         // this.classList.remove("selected");
-                //     } else {
-                //         durableSelected = value;
-                //         this.classList.add("selected");
-                //     }
-                //     break;
-                // case 'saver':
-                //     if (alreadySelected) {
-                //         saverSelected = 'all';
-                //         // this.classList.remove("selected");
-                //     } else {
-                //         saverSelected = value;
-                //         this.classList.add("selected");
-                //     }
-                //     break;
-
-            case 'specials':
-                console.log('before: [' + specialsSelected + ']');
-                if (alreadySelected) {
-                    console.log('removing from specialsSelected');
-                    var index = specialsSelected.indexOf(value);
-                    specialsSelected.splice(index, 1);
-                    this.classList.remove("selected");
-                } else {
-                    console.log('adding to specialsSelected');
-                    console.log(typeof (value));
-                    specialsSelected.push(value);
-                    this.classList.add("selected");
-                }
-                console.log('after: [' + specialsSelected + ']');
-                break;
+            this.classList.remove("selected");
+        } else {
+            if (this.getAttribute("data-value") == "favorites") {
+                favoritesToggled = true;
+            } else {
+                featuresSelected.push(value);
+            }
+            this.classList.add("selected");
         }
 
-        //todo attribute == specials, value == roamer/pusher/disabler etc.
+        showHeroes();
 
+    }
 
+    function showHeroes() {
 
         var affected = [];
-        // for (var i = 0; i < heroes.length; i++) {
-        //     if (
-        //         (roleSelected == 'all' || hasInside(heroes[i].roles, parseInt(roleSelected))) &&
-        //         (rangeSelected == 'all' || hasInside(heroes[i].range, parseInt(rangeSelected))) &&
-        //         (damageSelected == 'all' || hasInside(heroes[i].damage, parseInt(damageSelected))) &&
-        //         (aoesplashSelected == 'all' || heroes[i].aoesplash) &&
-        //         (roamerSelected == 'all' || heroes[i].roamer) &&
-        //         (initiatorSelected == 'all' || heroes[i].initiator) &&
-        //         (disablerSelected == 'all' || heroes[i].disabler) &&
-        //         (nukerSelected == 'all' || heroes[i].nuker) &&
-        //         (pusherSelected == 'all' || heroes[i].pusher) &&
-        //         (durableSelected == 'all' || heroes[i].durable) &&
-        //         (saverSelected == 'all' || heroes[i].saver) &&
-        //         (favoritesSelected == false || hasInside(favorites, heroes[i].id.toString()))
-        //     ) {
+
         for (var i = 0; i < heroes.length; i++) {
             if (
-                (roleSelected == 'all' || hasInside(heroes[i].roles, parseInt(roleSelected))) &&
-                (rangeSelected == 'all' || hasInside(heroes[i].range, parseInt(rangeSelected))) &&
-                (damageSelected == 'all' || hasInside(heroes[i].damage, parseInt(damageSelected))) &&
-                (favoritesSelected == false || hasInside(favorites, heroes[i].id.toString())) &&
-                ((specialsSelected.length == 0) || ( heroes[i].hasOwnProperty("specials") && arrayContainsArray(heroes[i].specials, specialsSelected) ) )
-            ) { 
-                // console.log(heroes[i].localized_name + ' ' + heroes[i].specials);
-
-                // console.log(heroes[i].hasOwnProperty("specials"));
-
-                // if (heroes[i].hasOwnProperty("specials") ) {
-                //     console.log(specialsSelected);
-                //     console.log(heroes[i].specials);
-
-
-                //     if (arrayContainsArray(heroes[i].specials, specialsSelected)) {
-                //         affected.push(heroes[i].id);
-                //     }
-
-                // } else {
-                    affected.push(heroes[i].id);
-                // }
+                (favoritesToggled == false || hasInside(favorites, heroes[i].id.toString())) &&
+                ((featuresSelected.length == 0) || (heroes[i].hasOwnProperty("features") && arrayContainsArray(heroes[i].features, featuresSelected)))
+            ) {
+                affected.push(heroes[i].id);
             }
         }
 
@@ -345,89 +176,14 @@ function mycallback(data) {
         for (var i = 0; i < items.length; i++) {
             var id = items[i].getAttribute("data-id");
             if (hasInside(affected, parseInt(id))) {
-                if (hasClass(items[i], "darken")) {
-                    items[i].classList.remove("darken");
-                }
+                items[i].classList.remove("darken");
                 items[i].classList.add("lighten");
             } else {
-                if (hasClass(items[i], "lighten")) {
-                    items[i].classList.remove("lighten");
-                }
+                items[i].classList.remove("lighten");
                 items[i].classList.add("darken");
             }
         }
     }
 }
 
-getHeroes('heroes.json', mycallback); //passing mycallback as a method
-
-
-
-
-
-// var filter = {
-//     address: 'USA',
-//     name: 'Mark'
-// };
-// var users = [{
-//         name: 'John',
-//         email: 'johnson@mail.com',
-//         age: 25,
-//         address: ['Canada', 'England']
-//     },
-//     {
-//         name: 'Tom',
-//         email: 'tom@mail.com',
-//         age: 35,
-//         address: ['USA', 'Canada', 'England']
-//     },
-//     {
-//         name: 'Mark',
-//         email: 'mark@mail.com',
-//         age: 28,
-//         address: ['Canada', 'Ua']
-//     },
-//     {
-//         name: 'Mark',
-//         email: 'mark@mail.com',
-//         age: 28,
-//         address: ['USA', 'England', 'Ua']
-//     }
-// ];
-
-// myFilter = function (item) {
-//     for (var key in filter) {
-//         if (item[key] === undefined || item[key] != filter[key])
-//             return false;
-//     }
-//     return true;
-// }
-
-// users = users.filter(myFilter);
-
-// console.log(users)
-
-
-
-/*
- * Returns TRUE if the first specified array contains all elements
- * from the second one. FALSE otherwise.
- *
- * @param {array} superset
- * @param {array} subset
- *
- * @returns {boolean}
- */
-// function arrayContainsArray(superset, subset) {
-//     return subset.every(function (value) {
-//         return (superset.indexOf(value) >= 0);
-//     });
-// }
-
-// var array1 = ['A', 'B', 'C', 'D', 'E'];
-// var array2 = ['B', 'C', 'E'];
-// var array3 = ['B', 'C', 'Z'];
-// var array4 = ['Z'];
-
-
-// console.log(arrayContainsArray(array1, array2));
+getHeroes('heroes.json', mycallback);
